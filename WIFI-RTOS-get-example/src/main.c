8add4679fdaf924538a7f79df02b3e49a647fbe1
wesleygas <wesleygl16@gmail.com>
2019-05-20 18:44:34 -0300

    first !

--- WIFI-RTOS-get-example/src/main.c
+++ WIFI-RTOS-get-example/src/main.c
@@ -7,10 +7,10 @@
 
 #define STRING_EOL "\r\n"
 #define STRING_HEADER "-- WINC1500 weather client example --" STRING_EOL \
-                      "-- " BOARD_NAME " --" STRING_EOL                  \
-                      "-- Compiled: "__DATE__                            \
-                      " "__TIME__                                        \
-                      " --" STRING_EOL
+											"-- " BOARD_NAME " --" STRING_EOL                  \
+											"-- Compiled: "__DATE__                            \
+											" "__TIME__                                        \
+											" --" STRING_EOL
 
 /** IP address of host. */
 uint32_t gu32HostIp = 0;
@@ -35,13 +35,13 @@ static bool gbHostIpByName = false;
 static bool gbTcpConnection = false;
 
 /** Server host name. */
-static char server_host_name[] = MAIN_SERVER_NAME;
+char server_host_name[30] = MAIN_SERVER_NAME;
 
 #define TASK_WIFI_STACK_SIZE (4096 / sizeof(portSTACK_TYPE))
 #define TASK_WIFI_STACK_PRIORITY (tskIDLE_PRIORITY)
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
-                                          signed char *pcTaskName);
+																					signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
 extern void vApplicationTickHook(void);
 extern void vApplicationMallocFailedHook(void);
@@ -51,13 +51,13 @@ extern void xPortSysTickHandler(void);
  * \brief Called if stack overflow during execution
  */
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
-                                          signed char *pcTaskName) {
-  printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
-  /* If the parameters have been corrupted then inspect pxCurrentTCB to
+																					signed char *pcTaskName) {
+	printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
+	/* If the parameters have been corrupted then inspect pxCurrentTCB to
 	 * identify which task has overflowed its stack.
 	 */
-  for (;;) {
-  }
+	for (;;) {
+	}
 }
 
 /**
@@ -73,30 +73,30 @@ extern void vApplicationTickHook(void) {
 }
 
 extern void vApplicationMallocFailedHook(void) {
-  /* Called if a call to pvPortMalloc() fails because there is insufficient
+	/* Called if a call to pvPortMalloc() fails because there is insufficient
 	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
 	internally by FreeRTOS API functions that create tasks, queues, software
 	timers, and semaphores.  The size of the FreeRTOS heap is set by the
 	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
 
-  /* Force an assert. */
-  configASSERT((volatile void *)NULL);
+	/* Force an assert. */
+	configASSERT((volatile void *)NULL);
 }
 
 /**
  * \brief Configure UART console.
  */
 static void configure_console(void) {
-  const usart_serial_options_t uart_serial_options = {
-      .baudrate = CONF_UART_BAUDRATE,
-      .charlength = CONF_UART_CHAR_LENGTH,
-      .paritytype = CONF_UART_PARITY,
-      .stopbits = CONF_UART_STOP_BITS,
-  };
+	const usart_serial_options_t uart_serial_options = {
+			.baudrate = CONF_UART_BAUDRATE,
+			.charlength = CONF_UART_CHAR_LENGTH,
+			.paritytype = CONF_UART_PARITY,
+			.stopbits = CONF_UART_STOP_BITS,
+	};
 
-  /* Configure UART console. */
-  sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
-  stdio_serial_init(CONF_UART, &uart_serial_options);
+	/* Configure UART console. */
+	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+	stdio_serial_init(CONF_UART, &uart_serial_options);
 }
 
 /* 
@@ -108,56 +108,56 @@ static void configure_console(void) {
  */
 /* http://www.cs.cmu.edu/afs/cs/academic/class/15213-f00/unpv12e/libfree/inet_aton.c */
 int inet_aton(const char *cp, in_addr *ap) {
-  int dots = 0;
-  register u_long acc = 0, addr = 0;
+	int dots = 0;
+	register u_long acc = 0, addr = 0;
 
-  do {
-    register char cc = *cp;
+	do {
+		register char cc = *cp;
 
-    switch (cc) {
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-      acc = acc * 10 + (cc - '0');
-      break;
+		switch (cc) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			acc = acc * 10 + (cc - '0');
+			break;
 
-    case '.':
-      if (++dots > 3) {
-        return 0;
-      }
-      /* Fall through */
+		case '.':
+			if (++dots > 3) {
+				return 0;
+			}
+			/* Fall through */
 
-    case '\0':
-      if (acc > 255) {
-        return 0;
-      }
-      addr = addr << 8 | acc;
-      acc = 0;
-      break;
+		case '\0':
+			if (acc > 255) {
+				return 0;
+			}
+			addr = addr << 8 | acc;
+			acc = 0;
+			break;
 
-    default:
-      return 0;
-    }
-  } while (*cp++);
+		default:
+			return 0;
+		}
+	} while (*cp++);
 
-  /* Normalize the address */
-  if (dots < 3) {
-    addr <<= 8 * (3 - dots);
-  }
+	/* Normalize the address */
+	if (dots < 3) {
+		addr <<= 8 * (3 - dots);
+	}
 
-  /* Store it if requested */
-  if (ap) {
-    ap->s_addr = _htonl(addr);
-  }
+	/* Store it if requested */
+	if (ap) {
+		ap->s_addr = _htonl(addr);
+	}
 
-  return 1;
+	return 1;
 }
 
 /**
@@ -169,11 +169,18 @@ int inet_aton(const char *cp, in_addr *ap) {
  * \return None.
  */
 static void resolve_cb(uint8_t *hostName, uint32_t hostIp) {
-  gu32HostIp = hostIp;
-  gbHostIpByName = true;
-  printf("resolve_cb: %s IP address is %d.%d.%d.%d\r\n\r\n", hostName,
-         (int)IPV4_BYTE(hostIp, 0), (int)IPV4_BYTE(hostIp, 1),
-         (int)IPV4_BYTE(hostIp, 2), (int)IPV4_BYTE(hostIp, 3));
+	gu32HostIp = hostIp;
+	if (0 == (int)IPV4_BYTE(hostIp, 0) && 0 == (int)IPV4_BYTE(hostIp, 0)) {
+		gethostbyname((uint8_t *)MAIN_SERVER_NAME);
+		return;
+	}
+	gbHostIpByName = true;
+	printf("resolve_cb: %s IP address is %d.%d.%d.%d\r\n\r\n", hostName,
+				 (int)IPV4_BYTE(hostIp, 0), (int)IPV4_BYTE(hostIp, 1),
+				 (int)IPV4_BYTE(hostIp, 2), (int)IPV4_BYTE(hostIp, 3));
+	sprintf(server_host_name, "%d.%d.%d.%d",
+					(int)IPV4_BYTE(hostIp, 0), (int)IPV4_BYTE(hostIp, 1),
+					(int)IPV4_BYTE(hostIp, 2), (int)IPV4_BYTE(hostIp, 3));
 }
 
 /**
@@ -187,66 +194,68 @@ static void resolve_cb(uint8_t *hostName, uint32_t hostIp) {
  */
 static void socket_cb(SOCKET sock, uint8_t u8Msg, void *pvMsg) {
 
-  /* Check for socket event on TCP socket. */
-  if (sock == tcp_client_socket) {
+	/* Check for socket event on TCP socket. */
+	if (sock == tcp_client_socket) {
 
-    switch (u8Msg) {
-    case SOCKET_MSG_CONNECT: {
-      printf("socket_msg_connect\n");
-      if (gbTcpConnection) {
-        memset(gau8ReceivedBuffer, 0, sizeof(gau8ReceivedBuffer));
-        sprintf((char *)gau8ReceivedBuffer, "%s", MAIN_PREFIX_BUFFER);
+		switch (u8Msg) {
+		case SOCKET_MSG_CONNECT: {
+			printf("socket_msg_connect\n");
+			if (gbTcpConnection) {
+				memset(gau8ReceivedBuffer, 0, sizeof(gau8ReceivedBuffer));
+				sprintf((char *)gau8ReceivedBuffer, "%s", MAIN_PREFIX_BUFFER);
 
-        tstrSocketConnectMsg *pstrConnect = (tstrSocketConnectMsg *)pvMsg;
-        if (pstrConnect && pstrConnect->s8Error >= SOCK_ERR_NO_ERROR) {
-          printf("send \n");
-          send(tcp_client_socket, gau8ReceivedBuffer, strlen((char *)gau8ReceivedBuffer), 0);
+				tstrSocketConnectMsg *pstrConnect = (tstrSocketConnectMsg *)pvMsg;
+				if (pstrConnect && pstrConnect->s8Error >= SOCK_ERR_NO_ERROR) {
+					printf("send \n");
+					send(tcp_client_socket, gau8ReceivedBuffer, strlen((char *)gau8ReceivedBuffer), 0);
 
-          memset(gau8ReceivedBuffer, 0, MAIN_WIFI_M2M_BUFFER_SIZE);
-          recv(tcp_client_socket, &gau8ReceivedBuffer[0], MAIN_WIFI_M2M_BUFFER_SIZE, 0);
-        } else {
-          printf("socket_cb: connect error!\r\n");
-          gbTcpConnection = false;
-          close(tcp_client_socket);
-          tcp_client_socket = -1;
-        }
-      }
-    } break;
+					memset(gau8ReceivedBuffer, 0, MAIN_WIFI_M2M_BUFFER_SIZE);
+					recv(tcp_client_socket, &gau8ReceivedBuffer[0], MAIN_WIFI_M2M_BUFFER_SIZE, 0);
+				} else {
+					printf("socket_cb: connect error!\r\n");
+					gethostbyname((uint8_t *)MAIN_SERVER_NAME);
+					gbTcpConnection = false;
+					close(tcp_client_socket);
+					tcp_client_socket = -1;
+				}
+			}
+		} break;
 
-    case SOCKET_MSG_RECV: {
-      char *pcIndxPtr;
-      char *pcEndPtr;
+		case SOCKET_MSG_RECV: {
+			char *pcIndxPtr;
+			char *pcEndPtr;
 
-      tstrSocketRecvMsg *pstrRecv = (tstrSocketRecvMsg *)pvMsg;
-      if (pstrRecv && pstrRecv->s16BufferSize > 0) {
-        printf(pstrRecv->pu8Buffer);
+			tstrSocketRecvMsg *pstrRecv = (tstrSocketRecvMsg *)pvMsg;
+			if (pstrRecv && pstrRecv->s16BufferSize > 0) {
+				printf("Recebi uma mensageeem!\r\n");
+				printf("pstrRecv->pu8Buffer: %s\r\n", pstrRecv->pu8Buffer);
+				delay_s(1);
+				memset(gau8ReceivedBuffer, 0, sizeof(gau8ReceivedBuffer));
+				recv(tcp_client_socket, &gau8ReceivedBuffer[0], MAIN_WIFI_M2M_BUFFER_SIZE, 0);
+			} else {
+				printf("socket_cb: recv error!\r\n");
+				close(tcp_client_socket);
+				tcp_client_socket = -1;
+			}
+		} break;
 
-        memset(gau8ReceivedBuffer, 0, sizeof(gau8ReceivedBuffer));
-        recv(tcp_client_socket, &gau8ReceivedBuffer[0], MAIN_WIFI_M2M_BUFFER_SIZE, 0);
-      } else {
-        printf("socket_cb: recv error!\r\n");
-        close(tcp_client_socket);
-        tcp_client_socket = -1;
-      }
-    } break;
-
-    default:
-      break;
-    }
-  }
+		default:
+			break;
+		}
+	}
 }
 
 static void set_dev_name_to_mac(uint8_t *name, uint8_t *mac_addr) {
-  /* Name must be in the format WINC1500_00:00 */
-  uint16 len;
+	/* Name must be in the format WINC1500_00:00 */
+	uint16 len;
 
-  len = m2m_strlen(name);
-  if (len >= 5) {
-    name[len - 1] = MAIN_HEX2ASCII((mac_addr[5] >> 0) & 0x0f);
-    name[len - 2] = MAIN_HEX2ASCII((mac_addr[5] >> 4) & 0x0f);
-    name[len - 4] = MAIN_HEX2ASCII((mac_addr[4] >> 0) & 0x0f);
-    name[len - 5] = MAIN_HEX2ASCII((mac_addr[4] >> 4) & 0x0f);
-  }
+	len = m2m_strlen(name);
+	if (len >= 5) {
+		name[len - 1] = MAIN_HEX2ASCII((mac_addr[5] >> 0) & 0x0f);
+		name[len - 2] = MAIN_HEX2ASCII((mac_addr[5] >> 4) & 0x0f);
+		name[len - 4] = MAIN_HEX2ASCII((mac_addr[4] >> 0) & 0x0f);
+		name[len - 5] = MAIN_HEX2ASCII((mac_addr[4] >> 4) & 0x0f);
+	}
 }
 
 /**
@@ -258,36 +267,35 @@ static void set_dev_name_to_mac(uint8_t *name, uint8_t *mac_addr) {
  * \return None.
  */
 static void wifi_cb(uint8_t u8MsgType, void *pvMsg) {
-  switch (u8MsgType) {
-  case M2M_WIFI_RESP_CON_STATE_CHANGED: {
-    tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
-    if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
-      printf("wifi_cb: M2M_WIFI_CONNECTED\r\n");
-      m2m_wifi_request_dhcp_client();
-    } else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
-      printf("wifi_cb: M2M_WIFI_DISCONNECTED\r\n");
-      gbConnectedWifi = false;
-      wifi_connected = 0;
-    }
+	switch (u8MsgType) {
+	case M2M_WIFI_RESP_CON_STATE_CHANGED: {
+		tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
+		if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
+			printf("wifi_cb: M2M_WIFI_CONNECTED\r\n");
+			m2m_wifi_request_dhcp_client();
+		} else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
+			printf("wifi_cb: M2M_WIFI_DISCONNECTED\r\n");
+			gbConnectedWifi = false;
+			wifi_connected = 0;
+		}
 
-    break;
-  }
+		break;
+	}
 
-  case M2M_WIFI_REQ_DHCP_CONF: {
-    uint8_t *pu8IPAddress = (uint8_t *)pvMsg;
-    printf("wifi_cb: IP address is %u.%u.%u.%u\r\n",
-           pu8IPAddress[0], pu8IPAddress[1], pu8IPAddress[2], pu8IPAddress[3]);
-    wifi_connected = M2M_WIFI_CONNECTED;
+	case M2M_WIFI_REQ_DHCP_CONF: {
+		uint8_t *pu8IPAddress = (uint8_t *)pvMsg;
+		printf("wifi_cb: IP address is %u.%u.%u.%u\r\n",
+					 pu8IPAddress[0], pu8IPAddress[1], pu8IPAddress[2], pu8IPAddress[3]);
+		wifi_connected = M2M_WIFI_CONNECTED;
 
-    /* Obtain the IP Address by network name */
-    // gethostbyname((uint8_t *)server_host_name);
-    break;
-  }
+		gethostbyname((uint8_t *)server_host_name);
+		break;
+	}
 
-  default: {
-    break;
-  }
-  }
+	default: {
+		break;
+	}
+	}
 }
 
 /**
@@ -295,79 +303,86 @@ static void wifi_cb(uint8_t u8MsgType, void *pvMsg) {
  * the whole report of free heap and total tasks status
  */
 static void task_monitor(void *pvParameters) {
-  static portCHAR szList[256];
-  UNUSED(pvParameters);
+	static portCHAR szList[256];
+	UNUSED(pvParameters);
 
-  for (;;) {
-    printf("--- task ## %u", (unsigned int)uxTaskGetNumberOfTasks());
-    vTaskList((signed portCHAR *)szList);
-    printf(szList);
-    vTaskDelay(1000);
-  }
+	for (;;) {
+		printf("--- task ## %u", (unsigned int)uxTaskGetNumberOfTasks());
+		vTaskList((signed portCHAR *)szList);
+		printf(szList);
+		vTaskDelay(1000);
+	}
 }
 
 static void task_wifi(void *pvParameters) {
-  tstrWifiInitParam param;
-  int8_t ret;
-  uint8_t mac_addr[6];
-  uint8_t u8IsMacAddrValid;
-  struct sockaddr_in addr_in;
+	tstrWifiInitParam param;
+	int8_t ret;
+	uint8_t mac_addr[6];
+	uint8_t u8IsMacAddrValid;
+	struct sockaddr_in addr_in;
+	int att_ip = 0;
 
-  /* Initialize the BSP. */
-  nm_bsp_init();
+	/* Initialize the BSP. */
+	nm_bsp_init();
 
-  /* Initialize Wi-Fi parameters structure. */
-  memset((uint8_t *)&param, 0, sizeof(tstrWifiInitParam));
+	/* Initialize Wi-Fi parameters structure. */
+	memset((uint8_t *)&param, 0, sizeof(tstrWifiInitParam));
 
-  /* Initialize Wi-Fi driver with data and status callbacks. */
-  param.pfAppWifiCb = wifi_cb;
-  ret = m2m_wifi_init(&param);
-  if (M2M_SUCCESS != ret) {
-    printf("main: m2m_wifi_init call error!(%d)\r\n", ret);
-    while (1) {
-    }
-  }
+	/* Initialize Wi-Fi driver with data and status callbacks. */
+	param.pfAppWifiCb = wifi_cb;
+	ret = m2m_wifi_init(&param);
+	if (M2M_SUCCESS != ret) {
+		printf("main: m2m_wifi_init call error!(%d)\r\n", ret);
+		while (1) {
+		}
+	}
 
-  /* Initialize socket module. */
-  socketInit();
+	/* Initialize socket module. */
+	socketInit();
 
-  /* Register socket callback function. */
-  registerSocketCallback(socket_cb, resolve_cb);
+	/* Register socket callback function. */
+	registerSocketCallback(socket_cb, resolve_cb);
 
-  /* Connect to router. */
-  printf("main: connecting to WiFi AP %s...\r\n", (char *)MAIN_WLAN_SSID);
-  m2m_wifi_connect((char *)MAIN_WLAN_SSID, sizeof(MAIN_WLAN_SSID), MAIN_WLAN_AUTH, (char *)MAIN_WLAN_PSK, M2M_WIFI_CH_ALL);
+	m2m_wifi_set_mac_address(gau8MacAddr);
 
-  addr_in.sin_family = AF_INET;
-  addr_in.sin_port = _htons(MAIN_SERVER_PORT);
-  inet_aton(MAIN_SERVER_NAME, &addr_in.sin_addr);
-  printf("Inet aton : %d", addr_in.sin_addr);
+	/* Connect to router. */
+	printf("main: connecting to WiFi AP %s...\r\n", (char *)MAIN_WLAN_SSID);
+	m2m_wifi_connect((char *)MAIN_WLAN_SSID, sizeof(MAIN_WLAN_SSID), MAIN_WLAN_AUTH, (char *)MAIN_WLAN_PSK, M2M_WIFI_CH_ALL);
 
-  while (1) {
-    m2m_wifi_handle_events(NULL);
+	addr_in.sin_family = AF_INET;
+	addr_in.sin_port = _htons(MAIN_SERVER_PORT);
+	//inet_aton(MAIN_SERVER_NAME, &addr_in.sin_addr);
 
-    if (wifi_connected == M2M_WIFI_CONNECTED) {
-      /* Open client socket. */
-      if (tcp_client_socket < 0) {
-        printf("socket init \n");
-        if ((tcp_client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-          printf("main: failed to create TCP client socket error!\r\n");
-          continue;
-        }
+	while (1) {
+		m2m_wifi_handle_events(NULL);
 
-        /* Connect server */
-        printf("socket connecting\n");
+		if (wifi_connected == M2M_WIFI_CONNECTED) {
+			/* Open client socket. */
+			if (tcp_client_socket < 0) {
+				printf("socket init \n");
+				if ((tcp_client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+					printf("main: failed to create TCP client socket error!\r\n");
+					continue;
+				}
+			}
 
-        if (connect(tcp_client_socket, (struct sockaddr *)&addr_in, sizeof(struct sockaddr_in)) != SOCK_ERR_NO_ERROR) {
-          close(tcp_client_socket);
-          tcp_client_socket = -1;
-          printf("error\n");
-        } else {
-          gbTcpConnection = true;
-        }
-      }
-    }
-  }
+			if (gbHostIpByName) {
+				/* Connect server */
+				printf("socket connecting\n\n");
+				inet_aton(server_host_name, &addr_in.sin_addr);
+				printf("Inet aton : %d\n", addr_in.sin_addr);
+				gbHostIpByName = false;
+				if (connect(tcp_client_socket, (struct sockaddr *)&addr_in, sizeof(struct sockaddr_in)) != SOCK_ERR_NO_ERROR) {
+					close(tcp_client_socket);
+					tcp_client_socket = -1;
+					printf("error\n");
+				} else {
+					printf("Ae, deu certo, TCP socket started\n");
+					gbTcpConnection = true;
+				}
+			}
+		}
+	}
 }
 /**
  * \brief Main application function.
@@ -377,22 +392,22 @@ static void task_wifi(void *pvParameters) {
  * \return Program return value.
  */
 int main(void) {
-  /* Initialize the board. */
-  sysclk_init();
-  board_init();
+	/* Initialize the board. */
+	sysclk_init();
+	board_init();
 
-  /* Initialize the UART console. */
-  configure_console();
-  printf(STRING_HEADER);
+	/* Initialize the UART console. */
+	configure_console();
+	printf(STRING_HEADER);
 
-  if (xTaskCreate(task_wifi, "Wifi", TASK_WIFI_STACK_SIZE, NULL,
-                  TASK_WIFI_STACK_PRIORITY, NULL) != pdPASS) {
-    printf("Failed to create Wifi task\r\n");
-  }
+	if (xTaskCreate(task_wifi, "Wifi", TASK_WIFI_STACK_SIZE, NULL,
+									TASK_WIFI_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create Wifi task\r\n");
+	}
 
-  vTaskStartScheduler();
+	vTaskStartScheduler();
 
-  while (1) {
-  };
-  return 0;
+	while (1) {
+	};
+	return 0;
 }
--- WIFI-RTOS-get-example/src/main.h
+++ WIFI-RTOS-get-example/src/main.h
@@ -48,26 +48,31 @@ extern "C" {
 #include "driver/include/m2m_wifi.h"
 
 /** Wi-Fi Settings */
-#define MAIN_WLAN_SSID "Insper_IoT"         /**< Destination SSID */
-#define MAIN_WLAN_AUTH M2M_WIFI_SEC_WPA_PSK /**< Security manner */
-#define MAIN_WLAN_PSK ""                    /**< Password for Destination SSID */
+#define MAIN_WLAN_SSID                    "asimov4" /**< Destination SSID */
+#define MAIN_WLAN_AUTH                    M2M_WIFI_SEC_WPA_PSK /**< Security manner */
+#define MAIN_WLAN_PSK                     "a ultima pergunta" /**< Password for Destination SSID */
+
+#define MAIN_MAC_ADDRESS                     {0x42, 0x00, 0x61, 0x43, 0xf9, 0x3f}
 
 /** Using broadcast address for simplicity. */
-#define MAIN_SERVER_PORT (5000)
+#define MAIN_SERVER_PORT                    (80)
 
 /** IP address parsing. */
-#define IPV4_BYTE(val, index) ((val >> (index * 8)) & 0xFF)
+#define IPV4_BYTE(val, index)               ((val >> (index * 8)) & 0xFF)
 
 /** Send buffer of TCP socket. */
-#define MAIN_PREFIX_BUFFER "GET / HTTP/1.1\r\n Accept: */*\r\n\r\n"
+#define MAIN_PREFIX_BUFFER                  "GET /?temp=10&fan=243&device=23 HTTP/1.1\r\nHost: insper.herokuapp.com\r\nAccept: */*\r\n\r\n"
+
 
 /** Weather information provider server. */
-#define MAIN_SERVER_NAME "192.168.50.63"
+#define MAIN_SERVER_NAME                    "insper.herokuapp.com"
 
 /** Receive buffer size. */
-#define MAIN_WIFI_M2M_BUFFER_SIZE 1400
+#define MAIN_WIFI_M2M_BUFFER_SIZE           1400
 
-#define MAIN_HEX2ASCII(x) (((x) >= 10) ? (((x)-10) + 'A') : ((x) + '0'))
+#define MAIN_HEX2ASCII(x)                   (((x) >= 10) ? (((x) - 10) + 'A') : ((x) + '0'))
+
+static uint8_t gau8MacAddr[] = MAIN_MAC_ADDRESS;
 
 #ifdef __cplusplus
 }
